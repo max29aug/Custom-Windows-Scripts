efa1da76c77acccf6d5534b714dc75d055051e0d
max29aug <144142135+max29aug@users.noreply.github.com>
2024-01-30 09:24:20 +0530

    Add files via upload

---
+++ Get-LastShutdownEvents.ps1
@@ -0,0 +1,18 @@
+﻿# Query the System Event Log for shutdown and restart events
+$events = Get-WinEvent -LogName System -FilterXPath @"
+    <QueryList>
+        <Query Id="0" Path="System">
+            <Select Path="System">
+                *[System[(EventID=1074 or EventID=1076 or EventID=6006 or EventID=6008) and TimeCreated[timediff(@SystemTime) &lt;= 604800000]]]
+            </Select>
+        </Query>
+    </QueryList>
+"@
+
+# Display the results
+foreach ($event in $events) {
+    $time = $event.TimeCreated
+    $eventId = $event.Id
+    $message = $event.Message
+    Write-Output "Event ID: $eventId, Time: $time, Message: $message"
+}
