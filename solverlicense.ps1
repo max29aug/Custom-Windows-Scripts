d790ac919a2b57d68a96e53e29c10c0c5afbf513
max29aug <144142135+max29aug@users.noreply.github.com>
2024-03-05 02:39:29 +0530

    Update solverlicense.ps1

--- solverlicense.ps1
+++ solverlicense.ps1
@@ -1,14 +1,16 @@
-# URL of the .reg file
+# URL of the .reg file on GitHub
 $regFileUrl = "https://raw.githubusercontent.com/max29aug/test/main/addlicense.reg"
 
 # Destination path to save the .reg file
-$regFilePath = "$env:TEMP\your.reg"
+$regFilePath = "$env:TEMP\example.reg"
 
 # Download the .reg file
 Invoke-WebRequest -Uri $regFileUrl -OutFile $regFilePath
 
-# Apply the .reg file silently
-Start-Process -FilePath "regedit.exe" -ArgumentList "/s", $regFilePath -Wait
-
-# Clean up: Delete the downloaded .reg file
-Remove-Item -Path $regFilePath
+# Apply the .reg file
+if (Test-Path $regFilePath) {
+    Start-Process -FilePath "regedit.exe" -ArgumentList "/s", $regFilePath -Wait
+    Write-Host "Registry keys from example.reg applied successfully."
+} else {
+    Write-Host "Failed to download the registry file."
+}
