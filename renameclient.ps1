5841daf3f87db7d2eeddb6647081e515ee437ff8
max29aug <144142135+max29aug@users.noreply.github.com>
2024-03-04 18:35:55 +0530

    Create renameclient.ps1

---
+++ renameclient.ps1
@@ -0,0 +1,22 @@
+# Rename Computer
+function Rename-Computer {
+    param (
+        [Parameter(Mandatory=$true)][string]$name
+    )
+    process {
+        try {
+            $computer = Get-WmiObject -Class Win32_ComputerSystem
+            $result = $computer.Rename($name)
+            switch($result.ReturnValue) {
+                0 { Write-Host "Success" }
+                5 { Write-Error "You need administrative rights to execute this cmdlet"; exit }
+                default { Write-Host "Error - return value of " $result.ReturnValue; exit }
+            }
+        } catch {
+            Write-Host "Exception occurred in Rename-Computer " $Error
+        }
+    }
+}
+
+# Call the function with the new name
+Rename-Computer -name "client10"
