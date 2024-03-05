98f1be4a384a63fd9824c6649048759ee09a30c5
max29aug <144142135+max29aug@users.noreply.github.com>
2024-03-05 20:27:49 +0530

    Update addsolverregistry.ps1

--- addsolverregistry.ps1
+++ addsolverregistry.ps1
@@ -1,13 +1,17 @@
-
 if((Test-Path -LiteralPath "HKCU:\Software\FrontlineSystems\User Info") -ne $true) {  New-Item "HKCU:\Software\FrontlineSystems\User Info" -force -ea SilentlyContinue };
 if((Test-Path -LiteralPath "HKCU:\Software\FrontlineSystems\AnalyticSolverPlatform\17.0") -ne $true) {  New-Item "HKCU:\Software\FrontlineSystems\AnalyticSolverPlatform\17.0" -force -ea SilentlyContinue };
-New-ItemProperty -LiteralPath 'HKCU:\Software\FrontlineSystems\User Info' -Name 'UID' -Value '' -PropertyType String -Force -ea SilentlyContinue;
-New-ItemProperty -LiteralPath 'HKCU:\Software\FrontlineSystems\User Info' -Name 'Email' -Value '' -PropertyType String -Force -ea SilentlyContinue;
-New-ItemProperty -LiteralPath 'HKCU:\Software\FrontlineSystems\User Info' -Name 'FullName' -Value '' -PropertyType String -Force -ea SilentlyContinue;
-New-ItemProperty -LiteralPath 'HKCU:\Software\FrontlineSystems\User Info' -Name 'Company' -Value '' -PropertyType String -Force -ea SilentlyContinue;
-New-ItemProperty -LiteralPath 'HKCU:\Software\FrontlineSystems\User Info' -Name 'Telephone' -Value '' -PropertyType String -Force -ea SilentlyContinue;
-New-ItemProperty -LiteralPath 'HKCU:\Software\FrontlineSystems\User Info' -Name 'UserType' -Value '' -PropertyType String -Force -ea SilentlyContinue;
-New-ItemProperty -LiteralPath 'HKCU:\Software\FrontlineSystems\User Info' -Name 'IndustryType' -Value '' -PropertyType String -Force -ea SilentlyContinue;
-New-ItemProperty -LiteralPath 'HKCU:\Software\FrontlineSystems\AnalyticSolverPlatform\17.0' -Name 'DelayRESTCalls' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
-New-ItemProperty -LiteralPath 'HKCU:\Software\FrontlineSystems\AnalyticSolverPlatform\17.0' -Name 'PreventRESTCalls' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
-New-ItemProperty -LiteralPath 'HKCU:\Software\FrontlineSystems\AnalyticSolverPlatform\17.0' -Name 'checkUpdate' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
+
+# Set registry values for FrontlineSystems\User Info
+Set-ItemProperty -Path "HKCU:\Software\FrontlineSystems\User Info" -Name "UID" -Value ""
+Set-ItemProperty -Path "HKCU:\Software\FrontlineSystems\User Info" -Name "Email" -Value ""
+Set-ItemProperty -Path "HKCU:\Software\FrontlineSystems\User Info" -Name "FullName" -Value ""
+Set-ItemProperty -Path "HKCU:\Software\FrontlineSystems\User Info" -Name "Company" -Value ""
+Set-ItemProperty -Path "HKCU:\Software\FrontlineSystems\User Info" -Name "Telephone" -Value ""
+Set-ItemProperty -Path "HKCU:\Software\FrontlineSystems\User Info" -Name "UserType" -Value ""
+Set-ItemProperty -Path "HKCU:\Software\FrontlineSystems\User Info" -Name "IndustryType" -Value ""
+
+# Set registry values for FrontlineSystems\AnalyticSolverPlatform\17.0
+Set-ItemProperty -Path "HKCU:\Software\FrontlineSystems\AnalyticSolverPlatform\17.0" -Name "DelayRESTCalls" -Value 1
+Set-ItemProperty -Path "HKCU:\Software\FrontlineSystems\AnalyticSolverPlatform\17.0" -Name "PreventRESTCalls" -Value 1
+Set-ItemProperty -Path "HKCU:\Software\FrontlineSystems\AnalyticSolverPlatform\17.0" -Name "checkUpdate" -Value 0
+
