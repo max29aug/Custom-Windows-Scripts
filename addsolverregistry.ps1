6d0041ddae391ba654910e7c05c2dc0cf0763c7b
max29aug <144142135+max29aug@users.noreply.github.com>
2024-03-05 20:23:06 +0530

    Update addsolverregistry.ps1

--- addsolverregistry.ps1
+++ addsolverregistry.ps1
@@ -1,4 +1,4 @@
-# Reg2CI (c) 2022 by Roger Zander
+
 if((Test-Path -LiteralPath "HKCU:\Software\FrontlineSystems\User Info") -ne $true) {  New-Item "HKCU:\Software\FrontlineSystems\User Info" -force -ea SilentlyContinue };
 if((Test-Path -LiteralPath "HKCU:\Software\FrontlineSystems\AnalyticSolverPlatform\17.0") -ne $true) {  New-Item "HKCU:\Software\FrontlineSystems\AnalyticSolverPlatform\17.0" -force -ea SilentlyContinue };
 New-ItemProperty -LiteralPath 'HKCU:\Software\FrontlineSystems\User Info' -Name 'UID' -Value '' -PropertyType String -Force -ea SilentlyContinue;
