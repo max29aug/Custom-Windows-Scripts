7475db8d71a0eaf6d8c298e80f17fb9707b174c3
max29aug <144142135+max29aug@users.noreply.github.com>
2024-11-14 00:56:45 +0530

    Create deleteinactiveuserprofile.ps1

---
+++ deleteinactiveuserprofile.ps1
@@ -0,0 +1,12 @@
+$daysToKeep = Days 
+$currentDate = Get-Date 
+$profiles = Get-WmiObject Win32_UserProfile | Where-Object { $_.Special -eq $false } 
+foreach ($profile in $profiles) { 
+$lastUseDate = $profile.ConvertToDateTime($profile.LastUseTime) 
+$daysSinceLastUse = ($currentDate - $lastUseDate).Days 
+if ($daysSinceLastUse -ge $daysToKeep) { 
+Remove-WmiObject -InputObject $profile 
+Write-Host "Deleted profile $($profile.LocalPath)" 
+} 
+} 
+Write-Host "Profile cleanup complete." 
