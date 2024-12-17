cca87ac96beefed741f4c450fde595ae6bbee5ca
max29aug <144142135+max29aug@users.noreply.github.com>
2024-12-18 00:21:25 +0530

    Update taskatlogoff.ps1

--- taskatlogoff.ps1
+++ taskatlogoff.ps1
@@ -7,8 +7,8 @@ $uncPath = "\\pdc\SharedApps\clear temporary files.ps1"
 # Create a task definition to clear temporary files
 $taskDef = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File \"$uncPath\""
 
-# Create a trigger to run the task at logoff
-$trigger = New-ScheduledTaskTrigger -AtLogOff
+# Create a trigger to run the task at logon with a delay
+$trigger = New-ScheduledTaskTrigger -AtLogOn -Delay 10s # Adjust the delay as needed
 
 # Register the task
 Register-ScheduledTask -Action $taskDef -Trigger $trigger -User $userSID -RunLevel Highest
